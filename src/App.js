1c36d79be939653e6594309fc5c2a31fdb95286c
Nkiriobasi <obasinkiri@gmail.com>
2022-05-26 14:23:02 -0700

    adjusted the media query for mobile version in hero section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-import CustomerSection from "./components/CustomerSection";
 
 function App() {
   return (
     <main className="App">
-      {/* <Navbar /> */}
-      <HeroSection />
-      <CustomerSection />
-      <HeroSection />
+      <Navbar />
       <HeroSection />
     </main>
   );
