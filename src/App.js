7b7751912096c758601986aae978c5145c1f9ccd
Nkiriobasi <obasinkiri@gmail.com>
2022-05-27 00:20:36 -0700

    added a business section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Businesses from "./components/Businesses";
 
 function App() {
   return (
     <main className="App">
       <Navbar />
       <HeroSection />
+      <Businesses />
     </main>
   );
 }
