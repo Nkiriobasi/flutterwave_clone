600727ed7afa97e8dd6776d4b29b9843349c8a2e
Nkiriobasi <obasinkiri@gmail.com>
2022-05-30 10:48:38 +0100

    completed the mobile view for the customer section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Hero from "./components/Hero";
 import Businesses from "./components/Businesses";
 import Payment from "./components/Payment";
 import Commerce from "./components/Commerce";
+import Customer from "./components/Customer";
 
 function App() {
   return (
     <main className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
+      <Customer />
       <Businesses />
       <Payment />
       <Commerce />
