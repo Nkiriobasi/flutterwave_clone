2af4955112dc40508652027f5147f0d6a72cf0c1
Nkiriobasi <obasinkiri@gmail.com>
2022-08-18 21:59:06 +0100

    added images to be used for this project

---
+++ src/constants/images.js
@@ -0,0 +1,64 @@
+import box from '../assets/box.png';
+import checkout from '../assets/checkout.png';
+import chipper from '../assets/chipper.svg';
+import ci from '../assets/ci.svg';
+import cm from '../assets/cm.svg';
+import dots from '../assets/dots.png';
+import eu from '../assets/eu.svg';
+import full from '../assets/full.svg';
+import gb from '../assets/gb.svg';
+import gh from '../assets/gh.svg';
+import globe from '../assets/globe.png';
+import ke from '../assets/ke.svg';
+import microsoftImg from '../assets/microsoft-img.webp';
+import microsoft from '../assets/microsoft.svg';
+import mtn from '../assets/mtn.svg';
+import mu from '../assets/mu.svg';
+import mw from '../assets/mw.svg';
+import ng from '../assets/ng.svg';
+import piggyvest from '../assets/piggyvest.svg';
+import rw from '../assets/rw.svg';
+import soloArtistImg from '../assets/solo-artist-img.webp';
+import tz from '../assets/tz.svg';
+import uber from '../assets/uber.svg';
+import ug from '../assets/ug.svg';
+import us from '../assets/us.svg';
+import wise from '../assets/wise.svg';
+import yangaBeauty from '../assets/yanga-beauty.webp';
+import za from '../assets/za.svg';
+import zm from '../assets/zm.svg';
+
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    box,
+    checkout,
+    chipper,
+    ci,
+    cm,
+    dots,
+    eu,
+    full,
+    gb,
+    gh,
+    globe,
+    ke,
+    microsoftImg,
+    microsoft,
+    mtn,
+    mu,
+    mw,
+    ng,
+    piggyvest,
+    rw,
+    soloArtistImg,
+    tz,
+    uber,
+    ug,
+    us,
+    wise,
+    yangaBeauty,
+    za,
+    zm
+}
