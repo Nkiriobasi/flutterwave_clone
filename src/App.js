aa2a614bb0b1c0d17307a550c4759cf4f471c747
Nkiriobasi <obasinkiri@gmail.com>
2022-08-18 21:12:21 +0100

    refactored the folder structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Businesses from "./components/Businesses";
-import Payment from "./components/Payment";
-import Commerce from "./components/Commerce";
-import Customer from "./components/Customer";
-import DarkCard from "./components/DarkCard";
-import Apis from "./components/Apis";
-import GlobalReach from "./components/GlobalReach";
-import Cl from "./components/Cl";
-import Footer from "./components/Footer";
+import './App.scss';
 
 function App() {
   return (
-    <main className="App">
-      <Navbar />
-      <Hero />
-      <Customer />
-      <Businesses />
-      <Payment />
-      <Commerce />
-      <DarkCard />
-      <Apis />
-      <GlobalReach />
-      <Cl />
-      <Footer />
-    </main>
+    <main className="App"></main>
   )
 }
 
