0c976e3e3f1043f993927a3d11f19f5eb85e13f1
Nkiriobasi <obasinkiri@gmail.com>
2022-08-24 18:09:31 +0100

    adjusted the overflowing in this section

--- src/container/Businesses/Businesses.jsx
+++ src/container/Businesses/Businesses.jsx
@@ -9,22 +9,22 @@ const Businesses = () => {
             <div>
                 <p className="businesses__intro mb-5 font-medium">Wea are trusted by <b>1 Million+</b> businesses</p>
                 <div className="businesses__list flex items-center flex-wrap justify-start">
-                    <div className="flex-initial py-5 md:px-6 px-5">
+                    <div className="flex-initial py-5 md:pr-12 pr-11">
                         <img src={images.uber} alt="uber logo" className="business__logo block" />
                     </div>
-                    <div className="flex-initial py-5 md:px-6 px-5">
+                    <div className="flex-initial py-5 md:pr-12 pr-11">
                         <img src={images.mtn} alt="mtn logo" className="business__logo block" />
                     </div>
-                    <div className="flex-initial py-5 md:px-6 px-5">
+                    <div className="flex-initial py-5 md:pr-12 pr-11">
                         <img src={images.chipper} alt="chipper logo" className="business__logo block" />
                     </div>
-                    <div className="flex-initial py-5 md:px-6 px-5">
+                    <div className="flex-initial py-5 md:pr-12 pr-11">
                         <img src={images.piggyvest} alt="piggyvest logo" className="business__logo block" />
                     </div>
-                    <div className="flex-initial py-5 md:px-6 px-5">
+                    <div className="flex-initial py-5 md:pr-12 pr-11">
                         <img src={images.wise} alt="" className="business__logo block" />
                     </div>
-                    <div className="flex-initial py-5 md:px-6 px-5">
+                    <div className="flex-initial py-5 md:pr-12 pr-11">
                         <img src={images.microsoft} alt="" className="business__logo block" />
                     </div>
                 </div>
